516412da167fc6b8a676171a00328f7c88f58fc9
isaisaisa <louisa@spahl-hamburg.de>
2014-11-02 18:17:01 +0100

    added ProcentType

--- KFragebearbeitung/src/main/java/Datatypes/ProcentType.java
+++ KFragebearbeitung/src/main/java/Datatypes/ProcentType.java
@@ -1,7 +1,32 @@
 package Datatypes;
 
+import java.awt.font.NumericShaper;
+
 /**
  * Created by Louisa on 02.11.2014.
  */
 public class ProcentType{
+    private int procent;
+
+    //getter and setter
+    public int getProcent() {
+        return procent;
+    }
+
+    public void setProcent(int procent) {
+        this.procent = procent;
+    }
+
+    //Constructor
+    private ProcentType(int procent){
+        if (procent >= 0 && procent <= 100){
+            this.procent = procent;
+        }else{
+            throw new IllegalArgumentException("this number is not in range");
+        }
+    }
+
+    public ProcentType valueOf(int procent){
+        return new ProcentType(procent);
+    }
 }
