44bc721e4e90e0e29608d817844d28464f9beaa0
Marjan Bachtiari <marjan.bachtiari@haw-hamburg.de>
2014-11-03 20:40:42 +0100

    Modul Update

---
+++ KModul/src/main/java/Component/KModul.java
@@ -0,0 +1,14 @@
+package Component;
+
+import Interface.IModul;
+import Interface.IPersistenceServices;
+
+/**
+ * Created by KamikazeOnRoad on 03.11.2014.
+ */
+public class KModul implements IModul {
+
+    public KModul(IPersistenceServices persistenceService) {
+
+    }
+}
---
+++ KModul/src/main/java/Interface/IModul.java
@@ -0,0 +1,7 @@
+package Interface;
+
+/**
+ * Created by KamikazeOnRoad on 03.11.2014.
+ */
+public interface IModul {
+}
